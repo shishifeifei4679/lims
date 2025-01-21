ef045880ebe808d5a6ca23a409a4802e5e201a59
guo <guowm@penghaisoft.com>
2025-01-21 13:15:33 +0800

    1

--- src/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: "制造管理平台",  // 系统名称
-  publicPath: "/mom/component/lims/",  // 公共路径，用于根目录部署还是非根目录部署，例如：/admin/
-  outputDir: "dist",  // 打包时文件的输出位置默认"dist", 例如：dist/admin
-  pathReplaceStr: '/component/lims'  // 当系统不在qiankun下运行时，左侧菜单路由需要替换掉前缀，如主应用的资源中配置的路由为/component/mes/team/mesTeam，修改为/team/mesTeam
-};
